d4d7006e31309e2c10cef18e0a5f30e1697cba71
xiebinIBM <xiebin@cn.ibm.com>
2025-11-03 14:47:07 +0800

    修改cloudbuild.yaml

--- app/crud.py
+++ app/crud.py
@@ -24,7 +24,6 @@ def get_by_query(db: Session, emp_no: str = None, name: str = None,
         query = query.filter(Employee.gender == gender)
     if hire_date:
         query = query.filter(Employee.hire_date == hire_date)
-        
     return query.all()
 
 def create(db: Session, obj: EmployeeCreate):
